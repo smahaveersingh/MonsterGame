4eb818037842371f6dedd8935001dc6a89c1a2a2
smahaveersingh <smahaveermadhu@gmail.com>
2020-08-19 20:38:43 +0100

    Initial Commit

---
+++ Monster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Monster
+//
+//  Created by Mahaveer Singh on 19/08/2020.
+//  Copyright © 2020 Mahaveer Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
